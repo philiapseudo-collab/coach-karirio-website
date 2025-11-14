2f9715b057b9652c3dc11b54274ff95f5f8f997c
philiapseudo-collab <philiapseudo@gmail.com>
2025-11-14 19:15:59 +0300

    Revert to server mode - hybrid not supported in this Astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
+  output: "server",
   adapter: vercel()
 });
