efa76c172be6fceb3261e88d6211d8b64fabcdb2
philiapseudo-collab <philiapseudo@gmail.com>
2025-11-14 18:06:32 +0300

    Fix Vercel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/serverless';
+
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/adapter-vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
